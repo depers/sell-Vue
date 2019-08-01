2a09f5589c800d1e2c07f3096ba42b25b1463479
depers <dev_fengxiao@163.com>
2019-08-01 22:45:55 +0800

    项目设计和准备工作

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,4 +1,5 @@
 'use strict'
+const express = require('express')
 const utils = require('./utils')
 const webpack = require('webpack')
 const config = require('../config')
@@ -93,3 +94,35 @@ module.exports = new Promise((resolve, reject) => {
     }
   })
 })
+
+
+var app = express()
+var appData = require('../data.json');
+var seller = appData.seller;
+var goods = appData.goods;
+var ratings = appData.ratings;
+
+var apiRoutes = express.Router();
+
+apiRoutes.get('/seller', function(req, res){
+  res.json({
+    errno: 0,
+    data: seller
+  });
+});
+
+apiRoutes.get('/goods', function(req, res){
+  res.json({
+    errno: 0,
+    data: goods
+  });
+});
+
+apiRoutes.get('/ratings', function(req, res){
+  res.json({
+    errno: 0,
+    data: ratings
+  });
+});
+
+app.use('/api', apiRoutes);
